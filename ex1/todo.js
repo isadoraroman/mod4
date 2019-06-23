b7aaf48192c37371f30a5a23c6ad9559755848d3
isadoraroman <isadora_roman-silva@estudante.sc.senai.br>
2019-06-23 01:48:12 -0300

    criamos uma função que verifica a idade e retorna se o usuário é ou não maior de 18

---
+++ ex1/todo.js
@@ -0,0 +1,13 @@
+function checaIdade(idade) {
+
+    return new Promise(function (resolve, rejeita) {
+
+        setTimeout(function () {
+
+            return idade >= 18 ? resolve() : rejeita();
+
+        }, 2000);
+
+    });
+
+}
